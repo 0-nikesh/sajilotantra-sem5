18abfb759ec9087cde9c2f3bacb2b85177a6ee91
nikesh <bhandarinikesh93@gmail.com>
2024-12-12 08:48:25 +0545

    improved splash

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animated_splash/flutter_animated_splash.dart';
 import 'package:sajilotantra/view/dashboard.dart';
 import 'package:sajilotantra/view/login.dart';
+import 'package:sajilotantra/view/register.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,160 +13,40 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  void _navigateToLogin() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Login()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              height: 400,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: -40,
-                    height: 400,
-                    width: width,
-                    child: FadeInUp(
-                        duration: Duration(seconds: 1),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage(
-                                      'assets/images/background.png'),
-                                  fit: BoxFit.fill)),
-                        )),
-                  ),
-                  Positioned(
-                    height: 200,
-                    width: width + 20,
-                    child: FadeInUp(
-                        duration: Duration(milliseconds: 1000),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                            image: AssetImage('assets/images/tantra-logo.png'),
-                          )),
-                        )),
-                  )
-                ],
-              ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/tantra-logo.png',
+              width: 300,
+              height: 200,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  FadeInUp(
-                      duration: Duration(milliseconds: 1500),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Color.fromRGBO(49, 39, 79, 1),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30),
-                      )),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  FadeInUp(
-                      duration: Duration(milliseconds: 1700),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.white,
-                            border: Border.all(
-                                color: Color.fromRGBO(196, 135, 198, .3)),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color.fromRGBO(196, 135, 198, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10),
-                              )
-                            ]),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  border: Border(
-                                      bottom: BorderSide(
-                                          color: Color.fromRGBO(
-                                              196, 135, 198, .3)))),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Username",
-                                    hintStyle:
-                                        TextStyle(color: Colors.grey.shade700)),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              child: TextField(
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Password",
-                                    hintStyle:
-                                        TextStyle(color: Colors.grey.shade700)),
-                              ),
-                            )
-                          ],
-                        ),
-                      )),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  FadeInUp(
-                      duration: Duration(milliseconds: 1700),
-                      child: Center(
-                          child: TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                "Forgot Password?",
-                                style: TextStyle(
-                                    color: Color.fromRGBO(196, 135, 198, 1)),
-                              )))),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  FadeInUp(
-                      duration: Duration(milliseconds: 1900),
-                      child: MaterialButton(
-                        onPressed: () {},
-                        color: Color.fromRGBO(49, 39, 79, 1),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        height: 50,
-                        child: Center(
-                          child: Text(
-                            "Login",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      )),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  FadeInUp(
-                      duration: Duration(milliseconds: 2000),
-                      child: Center(
-                          child: TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                "Create Account",
-                                style: TextStyle(
-                                    color: Color.fromRGBO(49, 39, 79, .6)),
-                              )))),
-                ],
-              ),
-            )
+            SizedBox(height: 20),
+            Image.asset(
+              'assets/images/text-logo.png',
+            ),
+            const SizedBox(height: 50),
+            const CircularProgressIndicator(
+              color: Color.fromARGB(255, 17, 47, 74),
+            ),
           ],
         ),
       ),
