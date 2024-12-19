24d617115a775fb63c67288c38828bc6e496764b
nikesh <bhandarinikesh93@gmail.com>
2024-12-19 22:57:15 +0545

    get theme added in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:sajilotantra/core/app_theme/app_theme.dart';
+import 'package:sajilotantra/view/dashboard.dart';
+import 'package:sajilotantra/view/login.dart';
+import 'package:sajilotantra/view/register.dart';
+import 'package:sajilotantra/view/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        initialRoute: "/",
+        debugShowCheckedModeBanner: false,
+        theme: getApplicationTheme(),
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/login': (context) => const Login(),
+          "/register": (context) => const Register(),
+          "/dashboard": (context) => const Dashboard(),
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sajilotantra/view/dashboard.dart';
-import 'package:sajilotantra/view/login.dart';
-import 'package:sajilotantra/view/register.dart';
-import 'package:sajilotantra/view/splash_screen.dart';
+import 'package:sajilotantra/app.dart';
 
 void main() {
-  runApp(MaterialApp(
-      initialRoute: "/",
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/login': (context) => Login(),
-        "/register": (context) => Register(),
-        "/dashboard": (context) => Dashboard(),
-      }));
+  runApp(const MyApp());
 }
