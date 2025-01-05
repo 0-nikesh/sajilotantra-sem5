be0d7bd5bd905d2205bf4bbb51cc4c2b0d7d2727
nikesh <bhandarinikesh93@gmail.com>
2025-01-05 12:21:09 +0545

    started get it for injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:sajilotantra/app.dart';
+import 'package:sajilotantra/services/service_locator.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependency();
+
+  runApp(
+    const MyApp(),
+  );
 }
