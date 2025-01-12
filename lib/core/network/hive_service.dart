df4b9fb737b865edf24cc373d23b7e2b66ecb4a2
nikesh <bhandarinikesh93@gmail.com>
2025-01-12 23:54:47 +0545

    created network folder inside lib for Clean Architecture

---
+++ lib/core/network/hive_service.dart
@@ -0,0 +1,5 @@
+//hive ko initiation directory path haru diney and db banaune,
+//ani tesma queries haru add garney like put request ko for add, 
+// yesmai resgister adapter ra open box ko concept use garnu parxa
+//register adapter le box imean table banauxa of database 
+// ani open box le tes bhitra ko data or row read garxa 
