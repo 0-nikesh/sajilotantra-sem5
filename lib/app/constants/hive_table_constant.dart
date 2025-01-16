1c3db2ef7802a933e98b74bd6d7f751524ee9f98
nikesh <bhandarinikesh93@gmail.com>
2025-01-17 01:48:06 +0545

    hive table constant created with usertable

--- lib/app/constants/hive_table_constant.dart
+++ lib/app/constants/hive_table_constant.dart
@@ -1,2 +1,9 @@
-//yeta hamle box haru create garxam 
+//yeta hamle box haru create garxam
 //with Hive table
+
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int userTableId = 0;
+  static const String userBox = 'userBox';
+}
