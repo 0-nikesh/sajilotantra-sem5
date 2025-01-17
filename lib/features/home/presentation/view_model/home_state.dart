f34d96aa0c58ad45e74a8ad1f79c43d2dcbc8d79
nikesh <bhandarinikesh93@gmail.com>
2025-01-17 09:53:23 +0545

    added cubit and state in home

---
+++ lib/features/home/presentation/view_model/home_state.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:sajilotantra/features/home/presentation/view/bottom_view/calendar.dart';
+import 'package:sajilotantra/features/home/presentation/view/bottom_view/document.dart';
+import 'package:sajilotantra/features/home/presentation/view/bottom_view/map.dart';
+import 'package:sajilotantra/features/home/presentation/view/bottom_view/setting.dart';
+
+import '../view/bottom_view/first.dart';
+
+class HomeState extends Equatable {
+  final int selectedIndex;
+  final List<Widget> views;
+
+  const HomeState({
+    required this.selectedIndex,
+    required this.views,
+  });
+
+  // Initial state
+  static HomeState initial() {
+    return const HomeState(
+      selectedIndex: 0,
+      views: [
+        HomeScreen(),
+        Calendar(),
+        MapScreen(),
+        Document(),
+        Setting(),
+      ],
+    );
+  }
+
+  // Copy state with changes
+  HomeState copyWith({
+    int? selectedIndex,
+    List<Widget>? views,
+  }) {
+    return HomeState(
+      selectedIndex: selectedIndex ?? this.selectedIndex,
+      views: views ?? this.views,
+    );
+  }
+
+  @override
+  List<Object?> get props => [selectedIndex, views];
+}
