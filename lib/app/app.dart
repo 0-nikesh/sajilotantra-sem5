44a613d92ea6a933398a43c75b312408117bcb74
nikesh <bhandarinikesh93@gmail.com>
2025-03-04 23:30:35 +0545

    with themes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -27,9 +27,53 @@ class MyApp extends StatelessWidget {
         title: 'Sajilotantra',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          brightness: Brightness.light,
           primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.grey[100],
+          cardColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 255, 255, 255),
+            foregroundColor: Colors.white,
+            elevation: 2,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          textTheme: const TextTheme(
+            headlineSmall: TextStyle(fontSize: 24, color: Colors.black87),
+            titleMedium: TextStyle(fontSize: 18, color: Colors.black87),
+            bodyLarge: TextStyle(fontSize: 16, color: Colors.black87),
+            bodyMedium: TextStyle(fontSize: 14, color: Colors.black87),
+          ),
+          useMaterial3: true, // Optional: Enables Material 3 design
         ),
-        home: const SplashScreen(), // Replace with your initial screen
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blueGrey,
+          scaffoldBackgroundColor: Colors.grey[900],
+          cardColor: Colors.grey[850],
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.grey[900],
+            foregroundColor: Colors.white,
+            elevation: 2,
+            titleTextStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          textTheme: const TextTheme(
+            headlineSmall: TextStyle(fontSize: 24, color: Colors.white),
+            titleMedium: TextStyle(fontSize: 18, color: Colors.white),
+            bodyLarge: TextStyle(fontSize: 16, color: Colors.white70),
+            bodyMedium: TextStyle(fontSize: 14, color: Colors.white70),
+          ),
+          useMaterial3: true, // Optional: Enables Material 3 design
+        ),
+        themeMode: ThemeMode.system, // Switch based on system settings
+        home: const SplashScreen(), // Your initial screen
       ),
     );
   }
